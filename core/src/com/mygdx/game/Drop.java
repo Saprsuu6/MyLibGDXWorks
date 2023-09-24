6b625b7ae3e5155cfbe05915348765cddd1dcc1f
Andry Saprigin <coffeei.2002@gmail.com>
2023-09-24 16:30:20 +0300

    Pause screen

--- core/src/com/mygdx/game/Drop.java
+++ core/src/com/mygdx/game/Drop.java
@@ -51,7 +51,11 @@ public class Drop extends Game {
 
         batch = new SpriteBatch();
         text = new BitmapFont();
-        //this.setScreen(new MainMenuScreen(this));
+        this.setScreen(new MainMenuScreen(this));
+        this.setScreen(new MainMenuScreen(this));
+        this.setScreen(new MainMenuScreen(this));
+
+        // some new changes
     }
 
     @Override
